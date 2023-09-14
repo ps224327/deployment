1f7b6c66ad0503fc7b75724df8ca3c4371594a40
ps224327 <74538995+ps224327@users.noreply.github.com>
2023-09-14 09:40:21 +0200

    Update ExampleTest.php

--- Web/tests/Unit/ExampleTest.php
+++ Web/tests/Unit/ExampleTest.php
@@ -1,16 +1,16 @@
-<?php
+// <?php
 
-namespace Tests\Unit;
+// namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+// use PHPUnit\Framework\TestCase;
 
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_that_true_is_true(): void
-    {
-        $this->assertTrue(true);
-    }
-}
+// class ExampleTest extends TestCase
+// {
+//     /**
+//      * A basic test example.
+//      */
+//     public function test_that_true_is_true(): void
+//     {
+//         $this->assertTrue(true);
+//     }
+// }
